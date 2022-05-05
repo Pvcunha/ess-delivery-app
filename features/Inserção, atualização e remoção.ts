674142c6e56819cef9aa57efe2c640e7420c8535
Zenio Oliveira <39632694+MisterZ99@users.noreply.github.com>
2022-05-05 16:27:22 -0300

    Testes de atuali, inser e rem de métodos

--- features/Inserção, atualização e remoção.ts
+++ features/Inserção, atualização e remoção.ts
@@ -3,85 +3,131 @@ import { browser, $, element, ElementArrayFinder, by } from 'protractor';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
 
+const baseURL = "http://localhost:4200/"
+
 let sleep = (ms => new Promise(resolve => setTimeout(resolve, ms)));
 
-let sameNumber = ((elem, number) => elem.element(by.name('numberlist')).getText().then(text => text === number));
+//let sameNumber = ((elem, number) => elem.element(by.name('numberlist')).getText().then(text => text === number));
 let sameName = ((elem, name) => elem.element(by.name('namelist')).getText().then(text => text === name));
-let sameType = ((elem, type) => elem.element(by.name('typelist')).getText().then(text => text === type));
+//let sameType = ((elem, type) => elem.element(by.name('typelist')).getText().then(text => text === type));
 
 let pAND = ((p,q) => p.then(a => q.then(b => a && b)))
-http://localhost:4200/home
+
+const goToPaymentPage = async(user_id) =>{
+    // vai para a o profile de cliente
+    // (vai apra /home)
+    console.log("\nEntrando na página /home...");
+    await browser.get(baseURL + "home");
+    sleep(5000)
+    console.log("...Done\n");
+    // (clica no login de cliente)
+    console.log(`\nClicando no botão de loging (user)...`);
+    await $("a[name='user']").click();
+    sleep(5000)
+    console.log("...Done\n");
+
+    // realiza o login 
+    // (preenche o id)
+    console.log("\nPreenchendo o input login...");
+    await $("input[id='id']").sendKeys(<string>user_id); // preenche o input login
+    sleep(5000)
+    console.log("...Done\n")
+    // (clica no botão de login)
+    console.log("\nClicando no botão login...")
+    await $("a[class='pink-link']").click(); // clica no botão login
+    sleep(5000)
+    console.log("...Done\n")
+
+    // (clica no botão ver métodos de pagamento)
+    console.log("\nClicando no botão 'ver métodos de pagamento'...")
+    await $("a[name='payment methods']").click(); // clica no botão login
+    sleep(5000)
+    console.log("...Done\n")
+}
+
 defineSupportCode(function ({ Given, When, Then}) {
 //--------------------------------------------Scenario: Inserção de métodos de pagamento-------------------------------------------------------------------------------------------------------------------------------
     Given(/^o usuário está na página de inserção de método de pagamento$/, async () => {
-        await browser.get("http://localhost:4200/home");
-        await expect(browser.getTitle()).to.eventually.equal('Home');
-        await element(by.buttonText('Cliente')).click();
-        await $("input[name='ID']").sendKeys('578dbdea');
-        await element(by.buttonText('Log in')).click();
-        await element(by.buttonText('Ver Métodos de Pagamento')).click();
-        await element(by.buttonText('Inserir método de pagamento')).click();
+        await goToPaymentPage('578dbdea');
+        sleep(5000);
+        await $("a[class='pink-link']").click();
+        sleep(5000);
     });
 
     When(/^ele conclui o procedimento de inserção$/, async () => {
-        await element(by.buttonText('Cartão de crédito')).click();//Poderia ser qualquer um dos outros tipos
-        await $("input[name='Número do Cartão (16) digitos']").sendKeys('1000200030004000');
-        await $("input[name='Nome do titular']").sendKeys('Monster Z');
-        await $("input[name='CVV (3 digitos)']").sendKeys('632');
-        await $("input[name='Bandeira (insira: visa ou master)']").sendKeys('master');
-        await $("input[name='Apelido do Cartão']").sendKeys('Zz');
-        await element(by.buttonText('Adicionar')).click();
+        await $("a[class='dark-link']").click();
+        sleep(5000);
+        await $("input[placeholder='Número do Cartão (16) digitos']").sendKeys('1000200030004000');
+        sleep(5000);
+        await $("input[placeholder='Nome do titular']").sendKeys('Monster Z');
+        sleep(5000);
+        await $("input[placeholder='CVV (3 digitos)']").sendKeys('632');
+        sleep(5000);
+        await $("input[placeholder='Bandeira (insira: visa ou master)']").sendKeys('master');
+        sleep(5000);
+        await $("input[placeholder='Apelido do Cartão']").sendKeys('Zz');
+        sleep(5000);
+        await $("a[class='confirm-link']").click();
+        sleep(5000);
     });
 
-    Then(/^o Id do usuário é solicitado$/, async () => {
+    /*Then(/^o Id do usuário é solicitado$/, async () => {
         var allnames : ElementArrayFinder = element.all(by.name('Id'));
+        sleep(5000);
         allnames.filter(elem => sameName(elem,'Id')).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-    });
+        sleep(5000);
+    });*/
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //--------------------------------------------Scenario: Remoção de métodos de pagamento-------------------------------------------------------------------------------------------------------------------------------
 Given(/^o usuário está na página de remoção de método de pagamento$/, async () => {
-    await browser.get("http://localhost:4200/home");
-        await expect(browser.getTitle()).to.eventually.equal('Home');
-        await element(by.buttonText('Cliente')).click();
-        await $("input[name='ID']").sendKeys('578dbdea');
-        await element(by.buttonText('Log in')).click();
-        await element(by.buttonText('Ver Métodos de Pagamento')).click();
-        await element(by.buttonText('Remover método de pagamento')).click();
+    await goToPaymentPage('578dbdea');
+    sleep(5000);
+    await $("a[class='leaked-link']").click();
+    sleep(5000);
 });
 
 When(/^ele conclui o procedimento de remoção$/, async () => {
-    await element(by.buttonText('Remover método')).click();
+    await $("a[class='pink-link']").click();
+    sleep(5000);
 });
 
-Then(/^o Id do usuário é solicitado$/, async () => {
+/*Then(/^o Id do usuário é solicitado$/, async () => {
     var allnames : ElementArrayFinder = element.all(by.name('Id'));
-    allnames.filter(elem => sameNumber(elem,'Id')).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
-});
+    sleep(5000);
+    allnames.filter(elem => sameName(elem,'Id')).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    sleep(5000);
+});*/
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 //--------------------------------------------Scenario: Atualização de métodos de pagamento-------------------------------------------------------------------------------------------------------------------------------
 Given(/^o usuário está na página de atualização de método de pagamento$/, async () => {
-    await browser.get("http://localhost:4200/home");
-    await expect(browser.getTitle()).to.eventually.equal('Home');
-    await element(by.buttonText('Cliente')).click();
-    await $("input[name='ID']").sendKeys('578dbdea');
-    await element(by.buttonText('Log in')).click();
-    await element(by.buttonText('Ver Métodos de Pagamento')).click();
-    await element(by.buttonText('Atualizar método de pagamento')).click();
+    await goToPaymentPage('578dbdea');
+    sleep(5000);
+    await $("a[class='dark-link']").click();
+    sleep(5000);
 });
 
 When(/^ele conclui o procedimento de atualização$/, async () => {
-    await element(by.buttonText('nubank(  ** 5555)')).click();
-    await $("a[name='PicPay(mileto@ess.com)']").click();
-    await $("input[name='Insira o nome do usuário']").sendKeys('Mileto');
-    await $("input[name='Insira o E-mail']").sendKeys('mm@ess.com');
-    await element(by.buttonText('Editar método')).click();
+    await $("a[class='pink-link']").click();
+    sleep(5000);
+    await $("select[id='mySelect']").click();
+    sleep(5000);
+    await $("option[value='PicPay(mileto@ess.com)']").click();
+    sleep(5000);
+    await $("input[placeholder='Insira o nome do usuário']").sendKeys('Mileto');
+    sleep(5000);
+    await $("input[placeholder='Insira o E-mail']").sendKeys('mm@ess.com');
+    sleep(5000);
+    await $("a[class='confirm-link']").click();
+    sleep(5000);
 });
 
 Then(/^o Id do usuário é solicitado$/, async () => {
     var allnames : ElementArrayFinder = element.all(by.name('Id'));
-    allnames.filter(elem => sameNumber(elem,'Id')).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    sleep(5000);
+    allnames.filter(elem => sameName(elem,'Id')).then(elems => expect(Promise.resolve(elems.length)).to.eventually.equal(1));
+    sleep(5000);
 });
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
